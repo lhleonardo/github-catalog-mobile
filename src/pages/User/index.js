f2add3b0fc6204ab957c19dd2451a9fc3fe4db91
Leonardo Braz <lhleonardo@hotmail.com>
2020-03-29 20:13:45 -0300

    Show starred repo's user

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,75 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {ActivityIndicator, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
-export default function User({navigate}) {
-  return <View />;
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  StarsInfo,
+  Label,
+  Loading,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
+
+export default function User({navigation, route}) {
+  const {user} = route.params;
+  navigation.setOptions({title: user.name});
+
+  const [stars, setStars] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function loadingUserData() {
+      setLoading(true);
+      const response = await api.get(`/users/${user.login}/starred`);
+
+      setStars(response.data);
+      setLoading(false);
+    }
+
+    loadingUserData();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Avatar source={{uri: user.avatar}} />
+        <Name>{user.name}</Name>
+        <Bio>{user.bio}</Bio>
+      </Header>
+
+      <StarsInfo>
+        <Icon name="stars" size={30} color="#5960c1" />
+        <Label>Repositórios favoritos</Label>
+      </StarsInfo>
+      {loading ? (
+        <Loading>
+          <ActivityIndicator size={50} color="#999" />
+        </Loading>
+      ) : (
+        <Stars
+          data={stars}
+          keyExtractor={(star) => String(star.id)}
+          renderItem={({item}) => (
+            <Starred>
+              <OwnerAvatar source={{uri: item.owner.avatar_url}} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}></Stars>
+      )}
+    </Container>
+  );
 }
---
+++ src/pages/User/styles.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Avatar = styled.Image`
+  width: 150px;
+  height: 150px;
+  border-radius: 75px;
+  /* cor que demonstra carregamento */
+  border-color: #eee;
+`;
+
+export const Name = styled.Text`
+  font-size: 25px;
+  margin-top: 10px;
+
+  color: #333;
+  text-align: center;
+  font-weight: bold;
+`;
+
+export const Bio = styled.Text`
+  font-size: 14px;
+  line-height: 18px;
+
+  color: #999;
+  margin-top: 5px;
+
+  text-align: center;
+`;
+
+export const StarsInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Label = styled.Text`
+  color: #333;
+  font-size: 16px;
+  font-weight: bold;
+
+  justify-content: center;
+
+  margin-left: 10px;
+`;
+
+export const Loading = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Stars = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+export const Starred = styled(RectButton)`
+  background: #e8e8e8;
+  border-radius: 4px;
+  padding: 10px 15px;
+  margin-bottom: 20px;
+  flex-direction: row;
+  align-items: center;
+`;
+export const OwnerAvatar = styled.Image`
+  width: 50px;
+  height: 50px;
+
+  border-radius: 25px;
+
+  background: #eee;
+`;
+export const Info = styled.View`
+  margin-left: 10px;
+  flex: 1;
+`;
+export const Title = styled.Text.attrs({numberOfLines: 1})`
+  font-size: 15px;
+  font-weight: bold;
+  color: #333;
+`;
+export const Author = styled.Text``;
