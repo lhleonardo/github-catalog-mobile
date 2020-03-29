d123ea2ceae957e7065e046da35b6d052c392edc
Leonardo Braz <lhleonardo@hotmail.com>
2020-03-29 02:20:59 -0300

    Configuring react-navigation (new version)

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, StatusBar} from 'react-native';
 
 import './config/ReactotronConfig';
 
+import Routes from './routes';
 const App = () => {
-  return (
-    <>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.welcome}>Bem vindo ao RN</Text>
-      </SafeAreaView>
-    </>
-  );
+  return <Routes />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <>
+      <StatusBar backgroundColor="#5960c1" barStyle="light-content" />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="main"
+          screenOptions={{
+            headerBackTitleVisible: false,
+            headerTitleAlign: 'center',
+            headerStyle: {
+              backgroundColor: '#5960c1',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 25,
+            },
+          }}>
+          <Stack.Screen
+            name="main"
+            component={User}
+            options={{title: 'Usuários'}}
+          />
+          <Stack.Screen
+            name="user"
+            component={Main}
+            options={{title: 'Usuário'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
